97d8de14106ebbc37ffdab9d26db7c0a8dd167cd
gechjs <gizachew980@gmail.com>
2024-10-25 13:47:40 +0300

    fix error

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
-import Header from "./components/Header.js";
-import Footer from "./components/Footer.js";
+
 import Main from "./components/Main.js";
 import Iphone from "./components/Iphone.js";
 import { Route, Routes } from "react-router-dom";
 import ProductPage from "./components/ProductPage.js";
+import SharedContent from "./components/SharedContent.js";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/iphone" element={<Iphone />} />
-        <Route path="/iphone/:productId" element={<ProductPage />}></Route>
+        <Route
+          path="/"
+          element={
+            <SharedContent>
+              <Main />
+            </SharedContent>
+          }
+        />
+        <Route
+          path="/iphone"
+          element={
+            <SharedContent>
+              <Iphone />
+            </SharedContent>
+          }
+        />
+        <Route
+          path="/iphone/:productId"
+          element={
+            <SharedContent>
+              <ProductPage />
+            </SharedContent>
+          }
+        ></Route>
         <Route
           path="*"
           element={
-            <div>
-              <br />
-              <br />
-              <h2>the page you are looking for can not be found</h2>
-            </div>
+            <SharedContent>
+              <div>
+                <br />
+                <br />
+                <h2>the page you are looking for can not be found</h2>
+              </div>
+            </SharedContent>
           }
         ></Route>
       </Routes>
-      <Footer />
     </>
   );
 }
